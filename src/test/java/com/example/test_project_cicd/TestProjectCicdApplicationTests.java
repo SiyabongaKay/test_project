851ed5b0c0d4c71c5d6ccafef2601ec6538ac1de
Siyabonga <khebevus@outlook.com>
2025-08-27 12:37:20 +0200

    first commit

---
+++ src/test/java/com/example/test_project_cicd/TestProjectCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_project_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
